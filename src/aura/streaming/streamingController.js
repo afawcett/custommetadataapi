cb0937994edf206fe447f7da4f3d009437ebaf66
Andrew Fawcett <andrewfawcett@Sarahs-MacBook.local>
2017-11-21 20:29:56 -0800

    Updated the build of the streaming component

--- src/aura/streaming/streamingController.js
+++ src/aura/streaming/streamingController.js
@@ -3,32 +3,58 @@
         var action = component.get("c.getSessionId");
         action.setCallback(this, function(response) {
         
-            // Configure CometD
+            // Configure CometD for this component
             var sessionId = response.getReturnValue();
             var cometd = new window.org.cometd.CometD();
             cometd.configure({
-                url: window.location.protocol + '//' + window.location.hostname + '/cometd/40.0/',
+                url: window.location.protocol + '//' + window.location.hostname + '/cometd/41.0/',
                 requestHeaders: { Authorization: 'OAuth ' + sessionId},
                 appendMessageTypeToURL : false
             });
+            cometd.websocketEnabled = false;
+            component.set('v.cometd', cometd);
 
             // Connect to 
             cometd.handshake($A.getCallback(function(status) {
                 if (status.successful) {
                     var eventName = component.get("v.channel");
-                    cometd.subscribe(eventName, $A.getCallback(function(message) {
+                    var subscription = 
+                        cometd.subscribe(eventName, $A.getCallback(function(message) {
                             var messageEvent = component.getEvent("onMessage");
-                            messageEvent.setParam("payload", message.data.payload);
-                            messageEvent.fire();
+	                        if(messageEvent!=null) {
+                                messageEvent.setParam("payload", message.data.payload);
+                                messageEvent.fire();                            
+	                        }
                         }
                     ));
+                    component.set('v.subscription', subscription);
                 } else {
-                    // TODO: Handle errors 
-                    console.log(status);
+                    // TODO: Throw an event / set error property here?
+                    console.error('streaming component: ' + status);
                 }
             }));
 
         });
         $A.enqueueAction(action);
+    },
+    handleDestroy : function (component, event, helper) {
+        // Ensure this component unsubscribes and disconnects from the server
+		var cometd = component.get("v.cometd");
+		var subscription = component.get("v.subscription");
+		cometd.unsubscribe(subscription, {}, function(unsubscribeReply) {
+		    if(unsubscribeReply.successful) {
+                cometd.disconnect(function(disconnectReply) 
+                    { 
+                        console.log('streaming component: Success unsubscribe')
+                        if(disconnectReply.successful) {
+                            console.log('streaming component: Success disconnect')
+                        } else {
+                            console.error('streaming component: Failed disconnect')                    
+                        }
+                    });
+		    } else {
+		        console.error('streaming component: Failed unsubscribe')                    		    
+		    }
+		});
     }
 })
